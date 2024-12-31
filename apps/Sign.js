dfa6b73eeed4c8b363bb057454111b25ef74007e
xiaodoubi <775146701@qq.com>
2024-12-31 11:44:07 +0000

    ^^^^
    Signed-off-by: xiaodoubi <775146701@qq.com>

--- apps/Sign.js
+++ apps/Sign.js
@@ -64,7 +64,7 @@ export class TextMsg extends plugin {
     <head>
       <meta charset="UTF-8" />
       <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-      <link rel="stylesheet" href="https://cdn.atxrom.com/logier/CSS/sign.css"> 
+      
       
       <style>
        html,
