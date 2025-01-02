dfc0d332d6d0fb0c23cce5c30ce9bb8d80597d7c
xiaodoubi <775146701@qq.com>
2025-01-02 21:55:53 +0800

    回滚

--- apps/Jrys.js
+++ apps/Jrys.js
@@ -118,7 +118,6 @@ async function generateFortune (e) {
 <html lang="zh-CN">
 <head>
   <title>运势页面</title>
-  <style>
 /* jrys.css */
 /* 定义自定义字体 */
 @font-face {
@@ -178,7 +177,6 @@ html {
     padding: 0;
 }
 
-  </style>
 
 </head>
 <body>
